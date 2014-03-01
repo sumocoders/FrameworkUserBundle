1c84204a69b8e760852c1721efbd971e1143c0b0
Tijs Verkoyen <tijs@verkoyen.eu>
2014-03-01 19:18:31 +0100

    Override the getParent()-method
    So we can overrule the templates inside FOSUserBundle with our own

--- SumoCodersFrameworkUserBundle.php
+++ SumoCodersFrameworkUserBundle.php
@@ -6,4 +6,14 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SumoCodersFrameworkUserBundle extends Bundle
 {
+    /**
+     * Override the getParent()-method so we can overrule the templates inside
+     * FOSUserBundle with our own
+     *
+     * @return string
+     */
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
