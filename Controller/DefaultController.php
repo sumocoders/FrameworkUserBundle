842c65fc0e4b3e88aee83090f34153f162a0d30a
Tijs Verkoyen <tijs@verkoyen.eu>
2014-06-23 17:47:39 +0200

    Made it possible to block a user.

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,11 +3,14 @@
 namespace SumoCoders\FrameworkUserBundle\Controller;
 
 use SumoCoders\FrameworkUserBundle\Form\AddUserType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -49,6 +52,72 @@ class DefaultController extends Controller
 
         return array(
             'form' => $form->createView(),
+        );
+    }
+
+    /**
+     * Block a user
+     *
+     * We won't delete users, as users can/will be linked through other stuff
+     * in our application.
+     *
+     * @Route("/block/{id}", requirements={"id"= "\d+"})
+     * @Method({"POST"})
+     * @Template()
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function blockAction(Request $request)
+    {
+        /** @var \Symfony\Component\Form\Extension\Csrf\CsrfProvider\CsrfTokenManagerAdapter $csrfProvider */
+        $csrfProvider = $this->get('form.csrf_provider');
+        /** @var \Symfony\Component\HttpFoundation\Session\Session $session */
+        $session = $this->get('session');
+        /** @var \Symfony\Bundle\FrameworkBundle\Translation\Translator $translator */
+        $translator = $this->get('translator');
+        $token = $request->get('token');
+        $id = (int) $request->get('id');
+
+        // validate our token
+        if (!$csrfProvider->isCsrfTokenValid('sumocoders_frameworkuser_default_block', $token)) {
+            $session->getFlashBag()->add(
+                'error',
+                $translator->trans('forms.errors.invalidToken')
+            );
+
+            return $this->redirect(
+                $this->generateUrl(
+                    'sumocoders_frameworkuser_default_edit',
+                    array('id' => $id)
+                )
+            );
+        }
+
+        /** @var \SumoCoders\FrameworkUserBundle\Model\FrameworkUserManager $userManager */
+        $userManager = $this->container->get('fos_user.user_manager');
+        /** @var \SumoCoders\FrameworkUserBundle\Entity\User $user */
+        $user = $userManager->findUserBy(array('id' => $id));
+
+        // validate the user
+        if (!$user) {
+            throw new NotFoundHttpException(
+                $translator->trans('core.errors.notFound')
+            );
+        }
+
+        $user->setEnabled(false);
+        $userManager->updateUser($user);
+
+        $session->getFlashBag()->add(
+            'success',
+            $translator->trans('user.flash.success.blocked', array('entity' => $user->getUsername()))
+        );
+
+        return $this->redirect(
+            $this->generateUrl(
+                'sumocoders_frameworkuser_default_index'
+            )
         );
     }
 }
