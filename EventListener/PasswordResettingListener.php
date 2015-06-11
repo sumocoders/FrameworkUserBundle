7b814d480489e59f8c0c2bb001195b0e0f016811
Tijs Verkoyen <tijs@verkoyen.eu>
2015-06-11 18:25:40 +0200

    Redirect to the login screen

---
+++ EventListener/PasswordResettingListener.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace SumoCoders\FrameworkUserBundle\EventListener;
+
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\FOSUserEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class PasswordResettingListener implements EventSubscriberInterface
+{
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $router;
+
+    /**
+     * @param UrlGeneratorInterface $router
+     */
+    public function __construct(UrlGeneratorInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FOSUserEvents::RESETTING_RESET_SUCCESS => 'onPasswordResettingSuccess',
+        );
+    }
+
+    public function onPasswordResettingSuccess(FormEvent $event)
+    {
+        $event->setResponse(
+            new RedirectResponse(
+                $this->router->generate(
+                    'fos_user_security_login'
+                )
+            )
+        );
+    }
+}
